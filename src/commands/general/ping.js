8f7d8075bd7f7ca4952d8af3df0e55228b8b56ce
joksulainen <minna1970.mn@gmail.com>
2021-05-21 19:53:11 +0300

    Added finishing reply edit

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -11,6 +11,7 @@ const handler = async (bot, interaction) => {
   await interaction.reply('Command received, measuring latency...');
   const msg = await interaction.channel.send('Ping...');
   msg.edit(`**Pong!** \`${Date.now() - msg.createdAt}ms\``);
+  interaction.editReply('Done!');
 };
 
 module.exports = {
