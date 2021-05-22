a5f8fa43b7e7c4d78c826366a328a3bdaf83aee8
joksulainen <minna1970.mn@gmail.com>
2021-05-22 16:27:54 +0300

    Modular commands in categories

--- src/commands/general/index.js
+++ src/commands/general/index.js
@@ -1,7 +1,17 @@
 'use strict';
 
-module.exports = {
-  info: require('./info'),
-  ping: require('./ping'),
-  rng: require('./rng'),
-};
+// Dependencies
+const fs = require('fs');
+const path = require('path');
+
+const commands = {};
+
+// Get dir contents without index.js
+const dirContents = fs.readdirSync(__dirname);
+dirContents.splice(dirContents.indexOf(path.basename(__filename)), 1);
+
+for (const file of dirContents) {
+  commands[path.basename(file, path.extname(file))] = require(`./${file}`);
+}
+
+module.exports = commands;
