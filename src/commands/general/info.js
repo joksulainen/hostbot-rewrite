0aaab29f7c9125d6583dd42a2f5e75ba8bc50b8b
joksulainen <minna1970.mn@gmail.com>
2021-07-08 23:50:18 +0300

    Add dynamic flag

--- src/commands/general/info.js
+++ src/commands/general/info.js
@@ -15,7 +15,7 @@ const handler = async (bot, interaction) => {
     .setColor(embedColor)
     .setTitle(`${appInfo.name} | Info`)
     .setDescription('The same bot you know and love rewritten in Node.js to bring more control and slash commands')
-    .setThumbnail(appInfo.iconURL())
+    .setThumbnail(appInfo.iconURL({ dynamic: true }))
     .setFooter(`Created by ${appInfo.owner.tag}`, appInfo.owner.displayAvatarURL({ dynamic: true }))
     .setTimestamp();
 
